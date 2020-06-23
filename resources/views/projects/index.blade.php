6e3171da534aecdf05c7f90dd12e840da68fb23b
PresKhaled <pres.kbayomy@gmail.com>
2020-06-23 23:08:07 +0200

    Laravel Birdboard

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Birdboard</title>
+</head>
+<body>
+    <h1>Projects</h1>
+    @foreach ($projects as $project)
+        <ul>
+            <li>{{ $project->title }}</li>
+            <li>{{ $project->description }}</li>
+        </ul>
+    @endforeach
+</body>
+</html>
